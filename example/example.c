f0fa24aad3d37988dc6035a636a246ee043f73b4
Lei <lynnl.yet@gmail.com>
2018-09-10 23:36:11 +0800

    Make KMOD_EXPLICIT_DECL default visibility
    You can NULLify _realmain and _antimain if you intended to develop a
    kernel extension library

--- example/example.c
+++ example/example.c
@@ -19,11 +19,15 @@ extern kern_return_t _stop(kmod_info_t *, void *);
 
 /* Will expand name if it's a macro */
 #define KMOD_EXPLICIT_DECL2(name, ver, start, stop) \
-	KMOD_EXPLICIT_DECL(name, ver, start, stop)
+	__attribute__((visibility("default")))          \
+        KMOD_EXPLICIT_DECL(name, ver, start, stop)
 
 KMOD_EXPLICIT_DECL2(BUNDLEID, KEXTBUILD_S, _start, _stop)
+
+/* If you intended to write a kext library  NULLify _realmain and _antimain */
 __private_extern__ kmod_start_func_t *_realmain = example_start;
 __private_extern__ kmod_stop_func_t *_antimain = example_stop;
+
 __private_extern__ int _kext_apple_cc = __APPLE_CC__;
 #endif
 
